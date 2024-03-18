f52117a0b120e5651487dc5709bbb310d9d9e0c2
mrilyew <99399973+mrilyew@users.noreply.github.com>
2024-03-18 21:52:48 +0300

    Add ability to sort photos by folders

--- scripts/RandomImages/main.py
+++ scripts/RandomImages/main.py
@@ -1,13 +1,15 @@
 import time
+from datetime import datetime
 import argparse
 import requests
 import os
 
 class ImageSearcher:
     # Setting API token
-    def __init__(self, token, timeout = 10):
-        self.token   = token
-        self.timeout = timeout
+    def __init__(self, token, timeout = 10, divide = 'days'):
+        self.token     = token
+        self.timeout   = timeout
+        self.divide_by = divide
 
     # Running search by photos
     def execute(self, q = "", count = 100):
@@ -21,11 +23,24 @@ class ImageSearcher:
         response = req['response']
         items = response['items']
         totalPhotos = []
+        folder = os.getcwd() + "/images"
+        def_folder = folder
+        write_info = open(os.getcwd() + "/posts.log", "a", encoding="utf-8")
 
-        write_info = open(os.getcwd() + "\\posts.log", "a", encoding="utf-8")
+        if os.path.isdir(folder) == False:
+            os.mkdir(folder)
 
-        if os.path.isdir(os.getcwd() + "\\images") == False:
-            os.mkdir(os.getcwd() + "\\images")
+        if self.divide_by == 'days' or self.divide_by == 'days_posts':
+            date = datetime.now()
+            folder += '/{0}-{1}-{2}'.format(str(date.month), str(date.day), date.year)
+            def_folder = folder
+        elif self.divide_by == 'posts':
+            pass
+        else:
+            pass
+
+        if os.path.isdir(folder) == False:
+            os.mkdir(folder)
 
         for item in items:
             attachments = item['attachments']
@@ -42,8 +57,22 @@ class ImageSearcher:
             for attachment in attachments:
                 if attachment['type'] != 'photo':
                     continue
+                
+                photo = attachment['photo']['sizes'][-1]['url']
+                image = requests.get(photo)
+                if self.divide_by == 'posts' or self.divide_by == 'days_posts':
+                    folder = def_folder + '/' + str(item['from_id']) + "_" + str(item['id'])
+                    if os.path.isdir(folder) == False:
+                        os.mkdir(folder)
+                
+                # Writing file
+                out = open(folder + '/' + os.path.basename(photo.split('?')[0]), "wb")
+                out.write(image.content)
+                out.close()
 
-                totalPhotos.append(attachment['photo']['sizes'][-1]['url'])
+                print('Downloaded ' + photo + ', sleeping for ' + str(self.timeout) + ' seconds\n')
+                time.sleep(self.timeout)
+
                 write_info.write(
                       '\nId: '   + str(attachment['photo']['owner_id']) + "_" + str(attachment['photo']['id'])
                     + '\n'
@@ -61,17 +90,6 @@ class ImageSearcher:
         print('Found ' + str(len(totalPhotos)) + ' photos. Downloading...\n')
         print('_________________________________\n')
 
-        for photo in totalPhotos:
-            image = requests.get(photo)
-
-            # Writing file
-            out = open(os.getcwd() + "\\images\\" + os.path.basename(photo.split('?')[0]), "wb")
-            out.write(image.content)
-            out.close()
-
-            print('Downloaded ' + photo + ', sleeping for ' + str(self.timeout) + ' seconds\n')
-            time.sleep(self.timeout)
-
         write_info.close()
 
 
@@ -81,11 +99,13 @@ parser.add_argument('--images_timeout', type=int, help='Timeout between download
 parser.add_argument('--timeout', type=int, help='Timeout between Newsfeed.get method call in seconds (10 by default).', default=10)
 parser.add_argument('--query', type=str, help='(Optional) Query param for Newsfeed.get method. Empty by default.', default='')
 parser.add_argument('--count', type=int, help='Count of posts. 100 by default.', default=100)
+parser.add_argument('--divide_by', type=str, help='Attachment division mode: \'days\', \'posts\', \'days_posts\', \'no\'', default='no')
 
 args = parser.parse_args()
 
 while True:
-    images = ImageSearcher(token=args.token, timeout=args.images_timeout)
-    images.execute(q=args.query + " has:photo", count=args.count)
+    has = ' has:photo'
+    images = ImageSearcher(token=args.token, timeout=args.images_timeout, divide=args.divide_by)
+    images.execute(q=args.query + has, count=args.count)
 
     time.sleep(args.timeout)
